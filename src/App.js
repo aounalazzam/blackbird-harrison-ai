93b279d1afae53612e6573b0e1c3f4d4db372f79
Aoun Alazzam <109635775+aounalazzam@users.noreply.github.com>
2023-03-11 17:06:35 +0200

    Updating To React 18

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import CssBaseline from '@mui/material/CssBaseline';
+/**
+ * This Source Code Edited By Aoun Alazzam
+ */
+
 import Grid from '@mui/material/Grid';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import LoginForm from './components/LoginForm';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme();
 
